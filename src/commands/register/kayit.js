8d97705e8704bc67748a078c9a1273e79a0b9ec1
frkn :) <79569914+respect0@users.noreply.github.com>
2022-03-17 15:34:32 +0300

    fix
    fix iste sorgulama

--- src/commands/register/kayit.js
+++ src/commands/register/kayit.js
@@ -6,15 +6,15 @@ moment.locale("tr")
 module.exports.run = async (client, message, args, embed) => {
     if (!roles.registerStaff.some(rol => message.member.roles.cache.has(rol)) && !message.member.permissions.has('ADMINISTRATOR')) return message.reply({ content: `Bu işlemi yapabilmek için yeterli yetkiye sahip değilsin.` }).delete(10);
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    if (message.author.id === member.id) return message.reply({ content: "Kendi üzerinde işlem uygulayamazsın." }).delete(10);
-    if (!member.manageable) return message.reply({ content: `Bu kişi üzerinde işlem uygulayamazsın.` }).delete(10);
-    if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply({ content: `Bu kişi seninle aynı yetkide veya senden daha yüksek bir yetkiye sahip.` }).delete(10);
     if (roles.manRoles.some(role => member.roles.cache.has(role)) || roles.womanRoles.some(role => member.roles.cache.has(role))) return message.reply({ content: `Bu kişi sunucuya zaten kayıt edilmiş.` }).delete(10);
     args = args.filter(a => a !== "" && a !== " ").splice(1);
     let setName;
     let isim = args.filter(arg => isNaN(arg)).map(arg => arg.charAt(0).replace('i', "İ").toUpperCase() + arg.slice(1)).join(" ");
     let yas = args.filter(arg => !isNaN(arg))[0] || undefined;
     if (!member || !isim || !yas) return message.reply({ content: `Hatalı kullanım, doğrusu; \`${settings.bot.prefix}kayıt [@respect/ID] <isim> <yaş>\`` })
+    if (message.author.id === member.id) return message.reply({ content: "Kendi üzerinde işlem uygulayamazsın." }).delete(10);
+    if (!member.manageable) return message.reply({ content: `Bu kişi üzerinde işlem uygulayamazsın.` }).delete(10);
+    if (message.member.roles.highest.position <= member.roles.highest.position) return message.reply({ content: `Bu kişi seninle aynı yetkide veya senden daha yüksek bir yetkiye sahip.` }).delete(10);
     setName = `${settings.guild.tagges.map(tag => member.user.tag.includes(tag)) ? settings.guild.nameTag : (settings.guild.defaultTag ? settings.guild.defaultTag : (settings.guild.defaultTag || ""))} ${isim} | ${yas}`;
     let nickData = {
         nick: setName,
